7916b0df6b4609df27c7c5c66fca09b82df0854a
nkradebeJHB <nkradebe@student.wethinkcode.co.za>
2022-10-18 18:15:46 -0700

    0

---
+++ _printf.c
@@ -0,0 +1,9 @@
+#include "main.h"
+/**
+ * _printf - produces output according to a format.
+ * @format: format string containing the characters.
+ * Return: The count of chars in format
+ */
+int _printf(const char *format, ...)
+{
+}
---
+++ main.h
@@ -0,0 +1,11 @@
+#ifndef _MAIN_H_
+#define _MAIN_H_
+
+#include <stdlib.h>
+#include <stdarg.h>
+
+int _putchar(char c);
+int _printf(const char *format, ...);
+
+
+#endif
