511b21ff581e85ca64be64bb07ee1304ec4197ee
nkradebeJHB <nkradebe@student.wethinkcode.co.za>
2022-10-20 12:53:04 -0700

    printf

--- _printf.c
+++ _printf.c
@@ -4,6 +4,28 @@
  * @format: format string containing the characters.
  * Return: The count of chars in format
  */
+static int (*check_for_specifiers(const char *format))(va_list)
+{
+	unsigned int i;
+	print_t p[] = {
+		{"c", print_c},
+		{"s", print_s},
+		{NULL, NULL}
+	};
+	for (i = 0; p[i].t != NULL; i++)
+	{
+		if (*p[i].t == *format)
+		{
+			break;
+		}
+	}
+	return (p[i].f);
+}
+/**                                                           
+ * _printf - produces output according to a format.
+ * @format: format string containing the characters.
+ * Return: The count of chars in format
+ */  
 int _printf(const char *format, ...)
 {
 }
